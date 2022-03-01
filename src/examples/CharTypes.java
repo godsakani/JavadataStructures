f49d47b1a1d983891c40721345eb408b2f847731
godsakani <nyonggodwill11@gmail.com>
2022-03-01 11:34:11 +0100

    taking quize and added task to chartpye class

---
+++ src/examples/CharTypes.java
@@ -0,0 +1,10 @@
+package examples;
+
+public class CharTypes {
+    public static void main(String[] args) {
+        for (int count = 1; count <  5; count++) {
+            count = count + count;
+        }
+        System.out.println();
+    }
+}
